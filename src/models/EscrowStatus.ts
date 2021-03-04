31067380ca6de26327bd9d35801ba313ad84c5b8
jasperdg <jasperdegooijer@gmail.com>
2021-03-04 14:55:35 +0100

    feat(escrow): adjusted EscrowStatus type to be up to date with current query result

--- src/models/EscrowStatus.ts
+++ src/models/EscrowStatus.ts
@@ -1,4 +1,11 @@
 export interface EscrowStatus {
     total_amount: string;
     type: 'valid_escrow' | 'invalid_escrow';
+    market: {
+        id: string,
+        description: string,
+        pool: {
+            collateral_token_id: string
+        }
+    }
 }
